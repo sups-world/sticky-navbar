80fb3f435274d0653ddf57b0e2efc6d06013d904
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-05 11:55:09 +0545

    trying some changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const navbarElement = document.querySelector(".navbar");
 
 const bottomContainerElement = document.querySelector(".bottom-container");
+
+const lineOneElement = document.getElementById("line-one");
+console.log("blue line::", lineOneElement.offsetTop);
+console.log(
+  "image wala::",
+  bottomContainerElement.offsetTop - navbarElement.offsetHeight - 50
+);
+
 // console.log(bottomContainerElement.offsetTop);
 
 // console.log(navbarElement.offsetHeight)
@@ -8,7 +16,7 @@ const bottomContainerElement = document.querySelector(".bottom-container");
 // console.log(navbarElement);
 
 window.addEventListener("scroll", () => {
-  //   console.log(window.scrollY);
+  console.log(window.scrollY);
   //   362
   //   50px is text margin that is added in css
   if (
@@ -16,7 +24,11 @@ window.addEventListener("scroll", () => {
     bottomContainerElement.offsetTop - navbarElement.offsetHeight - 50
   ) {
     navbarElement.classList.add("active");
+    //   } else if (window.scrollY > lineOneElement.offsetHeight) {
+    navbarElement.classList.remove("active");
+    navbarElement.classList.add("activeblue");
   } else {
     navbarElement.classList.remove("active");
+    navbarElement.classList.remove("activeblue");
   }
 });
